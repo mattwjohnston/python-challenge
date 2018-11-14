f4c0dd3fed2fe306d3f4bd57af2120cdfc5066bf
Matt Johnston <matt@mwjohnston.com>
2018-11-13 18:34:05 -0800

    finished pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+import operator
+
 csvpath = os.path.join('', 'PyPoll/Resources', 'election_data.csv')
 
 totalVotes = 0
@@ -11,8 +13,11 @@ with open(csvpath, newline='') as f:
   csvreader = csv.reader(f, delimiter=',')
   next(csvreader, None)
   for row in csvreader:
-
+    if row[2] not in candidates:
+      candidates.append(row[2])
+      candidateVotes[row[2]] = 0
     candidateVotes[row[2]] += 1
+    totalVotes += 1
 
 
 
@@ -20,5 +25,8 @@ print("Election Results")
 print("___________________________")
 print(f"Total Votes: {totalVotes}")
 print("___________________________")
-print(candidates)
-print(candidateVotes)
+for key, value in candidateVotes.items():
+  print(f"{key}: {round(value/totalVotes*100, 4)}% ({value})")
+print("___________________________")
+print("Winner: "+max(candidateVotes.items(), key=operator.itemgetter(1))[0])
+print("___________________________")
